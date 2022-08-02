8cb8f1a9b7884a7fa134bf9c5479aca51f25d138
大石头 <nnhy@vip.qq.com>
2022-08-02 13:16:56 +0800

    施耐德PLC协议

---
+++ NewLife.Schneider/Drivers/SchneiderParameter.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel;
+using NewLife.IoT.Drivers;
+
+namespace NewLife.Schneider.Drivers;
+
+/// <summary>Melsec参数</summary>
+public class SchneiderParameter : IDriverParameter
+{
+    /// <summary>地址。例如 127.0.0.1:6000</summary>
+    [Description("地址。例如 127.0.0.1:6000")]
+    public String Address { get; set; }
+
+    /// <summary>数据格式。ABCD/BADC/CDAB/DCBA</summary>
+    [Description("数据格式。ABCD/BADC/CDAB/DCBA")]
+    public String DataFormat { get; set; }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,58 @@
+﻿using NewLife.IoT.Drivers;
+using NewLife.IoT.ThingModels;
+using NewLife.Schneider.Drivers;
+
+Console.WriteLine("服务端地址默认为：127.0.0.1:6000，保持默认请回车开始连接，否则请输入服务端地址：");
+var address = Console.ReadLine();
+
+if (address == null || address == "") address = "127.0.0.1:6000";
+
+var driver = new SchneiderDriver();
+var pm = new SchneiderParameter { Address = address };
+var node = driver.Open(null, pm);
+
+// 测试打开两个通道
+node = driver.Open(null, pm);
+
+Console.WriteLine($"连接成功=>{address}！");
+
+Console.WriteLine("请输入整数值，按q退出：");
+
+var str = Console.ReadLine();
+
+do
+{
+    // 写入
+    var data = BitConverter.GetBytes(Int32.Parse(str));
+    var point = new Point
+    {
+        Name = "test",
+        Address = "D100:50",
+        Type = "Int32",
+        Length = data.Length
+    };
+
+    var res = driver.Write(node, point, data);
+
+    // 读取
+    var dic = driver.Read(node, new[] { point });
+    var data1 = dic[point.Name] as Byte[];
+
+    Console.WriteLine($"读取结果：{BitConverter.ToInt32(data1)}");
+    Console.WriteLine($"");
+    Console.WriteLine("请输入整数值，按q退出：");
+
+} while ((str = Console.ReadLine()) != "q");
+
+// 断开连接
+driver.Close(node);
+driver.Close(node);
+
+
+public class Point : IPoint
+{
+    public String Name { get; set; }
+    public String Address { get; set; }
+    public String Type { get; set; }
+    public Int32 Length { get; set; }
+}
