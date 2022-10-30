ee5bae4509e8d2c9d8416fdc805ec15abd36c570
笑笑 <xxred0324@qq.com>
2022-10-30 18:15:45 +0800

    添加循环读取测试模式

--- Test/Program.cs
+++ Test/Program.cs
@@ -1,4 +1,5 @@
-﻿using NewLife.IoT.Drivers;
+﻿using NewLife;
+using NewLife.IoT.Drivers;
 using NewLife.IoT.Protocols;
 using NewLife.IoT.ThingModels;
 using NewLife.Schneider.Drivers;
@@ -9,11 +10,12 @@ var address = Console.ReadLine();
 if (address == null || address == "") address = "127.0.0.1:502";
 
 var driver = new SchneiderDriver();
-var pm = new SchneiderParameter {
-    Host = 1, 
+var pm = new SchneiderParameter
+{
+    Host = 1,
     Server = address,
-    ReadCode = FunctionCodes.ReadRegister,
-    WriteCode = FunctionCodes.WriteRegister
+    ReadCode = FunctionCodes.ReadCoil,
+    WriteCode = FunctionCodes.WriteCoil
 };
 var node = driver.Open(null, pm);
 
@@ -22,33 +24,57 @@ node = driver.Open(null, pm);
 
 Console.WriteLine($"连接成功=>{address}！");
 
-Console.WriteLine("请输入整数值，按q退出：");
+Console.WriteLine($"读写模式输入1，循环读输入2：");
 
-var str = Console.ReadLine();
+var mode = Console.ReadLine();
+
+var str = "0";
+
+if (mode == "1")
+{
+    Console.WriteLine("请输入整数值，按q退出：");
+    str = Console.ReadLine();
+}
+
+var point = new Point
+{
+    Name = "test",
+    Address = "100",
+    Type = "UInt16",
+    Length = 2
+};
 
 do
 {
-    // 写入
-    var data = BitConverter.GetBytes(Int32.Parse(str));
-    var point = new Point
+    if (mode == "1")
     {
-        Name = "test",
-        Address = "100",
-        Type = "Int32",
-        Length = data.Length
-    };
+        // 写入
+        var data = BitConverter.GetBytes(Int32.Parse(str));
 
-    var res = driver.Write(node, point, data);
+        var res = driver.Write(node, point, data);
 
-    // 读取
-    var dic = driver.Read(node, new[] { point });
-    var data1 = dic[point.Name] as Byte[];
+        // 读取
+        var dic = driver.Read(node, new[] { point });
+        var data1 = dic[point.Name] as Byte[];
 
-    Console.WriteLine($"读取结果：{BitConverter.ToInt32(data1)}");
-    Console.WriteLine($"");
-    Console.WriteLine("请输入整数值，按q退出：");
-
-} while ((str = Console.ReadLine()) != "q");
+        Console.WriteLine($"读取结果：{BitConverter.ToInt32(data1)}");
+        Console.WriteLine($"");
+        Console.WriteLine("请输入整数值，按q退出：");
+    }
+    else
+    {
+         // 读取
+        var dic = driver.Read(node, new[] { point });
+        var data1 =  dic[point.Name] as Byte[];
+        //var res = BitConverter.ToInt32(data1);
+        var res = data1?.Swap(true, false).ToInt();
+        Console.WriteLine($"读取结果：{res}");
+        Console.WriteLine($"");
+        Thread.Sleep(1000);
+    }
+} while (
+(mode == "1" && (str = Console.ReadLine()) != "q")
+|| mode == "2");
 
 // 断开连接
 driver.Close(node);
